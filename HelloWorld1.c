5e9d1fcedd73e9b5c0806489ed2dc8bc309a4abd
AndreyCi <81033729+AndreyCi@users.noreply.github.com>
2021-03-20 19:09:36 +0300

    Add files via upload

---
+++ HelloWorld1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main ()
+{
+	printf("Hello world,\nhelloooo woooorld !\n"); //вывод на экран
+	printf("Hello, C!");
+	getch(); /* ожидаем нажатия 
+	многострочный комментарий
+		Enter */
+  return 0;		
+}
